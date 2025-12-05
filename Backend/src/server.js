0f78824285d9e807c90fade8dbc02100f4748d2a
TommyTeaVee <fihlatv@gmail.com>
2025-12-05 20:49:08 +0200

    try to serve static

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,10 +2,22 @@ require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = require("./app");
-const seedAdmin = require("./auth/seedAdmin");
-const seedModel = require("./auth/modelSeed")
-const seedSubs = require('./auth/subscriberseed')
-const dbS3Seed = require("./auth/dbS3Seeds")
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Serve Frontend
+app.use(express.static(path.join(__dirname, "../../Frontend/build/index.htm")));
+
+// Catch-all: send React's index.html for ANY route not starting with /api
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../Frontend/build/index.html"));
+});
+//const seedAdmin = require("./auth/seedAdmin");
+//const seedModel = require("./auth/modelSeed")
+//const seedSubs = require('./auth/subscriberseed')
+//const dbS3Seed = require("./auth/dbS3Seeds")
 
 const PORT = process.env.PORT || 5050;
 
@@ -17,7 +29,7 @@ const PORT = process.env.PORT || 5050;
     await seedSubs();
    await dbS3Seed()} */
   try {
-      seedAdmin()
+     // seedAdmin()
     app.listen(PORT, () => console.log(`🚀 Backend on http://localhost:${PORT}`)); } catch (e) {
     console.error("Failed to start:", e);
     process.exit(1);
