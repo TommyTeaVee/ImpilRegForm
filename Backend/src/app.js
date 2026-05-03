27771364aaee1e595d9215d77d1eb63e961dadd5
TommyTeaVee <fihlatv@gmail.com>
2026-05-03 21:01:27 +0200

    debugger cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,8 @@ app.use(express.json({limit:"500mb"}));
 // ALLOW YOUR FRONTEND
 const cors_origin = process.env.CORS_ORIGIN
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : []
+console.log("ALLOWED_ORIGINS RAW:", process.env.ALLOWED_ORIGINS);
+console.log("ALLOWED_ORIGINS ARRAY:", allowedOrigins);
 //app.use(cors({cors_origin}));
 app.use(cors({
   origin: cors_origin,
