336aba318587333e3bd4999163a650facf79346e
TommyTeaVee <fihlatv@gmail.com>
2026-05-03 20:01:43 +0200

    fixed cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,12 +9,23 @@ app.use(express.json({limit:"500mb"}));
  app.use(express.urlencoded({ extended: true, limit: "500mb" }));
  
 // ALLOW YOUR FRONTEND
-const cors_origin = process.env.CORS_ORIGIN
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : []
-//app.use(cors({cors_origin}));
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",").map(o => o.trim())
+  : [];
+
 app.use(cors({
-  origin: cors_origin,
-  credentials: true
+  origin: function (origin, callback) {
+    console.log("Incoming origin:", origin);
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 app.use(express.json());
 
